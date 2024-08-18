55544933c622dd322f621d09d21219955a4bfe7b
Connor Wales <connorwales@gmail.com>
2024-08-18 15:19:42 -0700

    add code block elements via SlateCustomElements.tsx, unresolved ts errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import SlateInput from "@/components/SlateInput";
+import SlateCustomElements from "@/components/SlateCustomElements";
 
 export default function Home() {
   return (
     <main>
       <SlateInput />
+      <SlateCustomElements />
     </main>
   );
 }
---
+++ components/SlateCustomElements.tsx
@@ -0,0 +1,73 @@
+"use client";
+// Import React dependencies.
+import React, { useState, useCallback } from 'react'
+// Import the Slate editor factory.
+import { Editor, Transforms, Element, createEditor } from 'slate'
+
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact } from 'slate-react'
+
+const initialValue = [
+  {
+    type: 'paragraph',
+    children: [{ text: 'A line of text in a paragraph.' }],
+  },
+]
+
+// Define a React component renderer for our code blocks.
+const CodeElement = props => {
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  )
+}
+
+const DefaultElement = props => {
+  return <p {...props.attributes}>{props.children}</p>
+}
+
+export default function SlateCustomElements() {
+
+
+  const [editor] = useState(() => withReact(createEditor()))
+
+  // Define a rendering function based on the element passed to `props`. We use
+  // `useCallback` here to memoize the function for subsequent renders.
+  const renderElement = useCallback(props => {
+    switch (props.element.type) {
+      case 'code':
+        return <CodeElement {...props} />
+      default:
+        return <DefaultElement {...props} />
+    }
+  }, [])
+
+  return (
+    // Add the editable component inside the context.
+    
+    <Slate 
+      editor={editor} 
+      initialValue={initialValue}
+    >
+      <Editable 
+        renderElement={renderElement}
+        onKeyDown={event => {
+          if (event.key === '`' && event.ctrlKey) {
+            event.preventDefault()
+            // Determine whether any of the currently selected blocks are code blocks.
+            const [match] = Editor.nodes(editor, {
+              match: n => n.type === 'code',
+            })
+            // Toggle the block type depending on whether there's already a match.
+            Transforms.setNodes(
+              editor,
+              { type: match ? 'paragraph' : 'code' },
+              { match: n => Element.isElement(n) && Editor.isBlock(editor, n) }
+            )
+          }
+        }}
+      />
+    </Slate>
+  )
+}
