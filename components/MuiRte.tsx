f00e5a08153177efd11f12e1174e6b380570df55
Connor Wales <connorwales@gmail.com>
2024-08-28 14:54:12 -0700

    refactor MuiRte and add additional experiments and examples

--- components/MuiRte.tsx
+++ components/MuiRte.tsx
@@ -1,10 +1,74 @@
 "use client";
 import MUIRichTextEditor from "mui-rte";
+import Chip from "@mui/material/Chip";
+import { ReactNode } from "react";
+import TableChartIcon from '@mui/icons-material/TableChart';
+import InvertColorsIcon from "@mui/icons-material/InvertColors";
+import TableViewIcon from '@mui/icons-material/TableView';
+import { useTheme } from "@mui/material";
 
-export default function MuiRte() {
+function MyChip(props: {children: ReactNode}) {
   return (
-    <MUIRichTextEditor 
-      label="MUIRichTextEditor"
+    <Chip
+      variant="outlined"
+      size="small"
+      color="primary"
+      label={props.children}
     />
   )
 }
+
+export default function MuiRte() {
+
+  const theme = useTheme();
+
+  return (
+    <>
+      <MUIRichTextEditor 
+        label="MUIRichTextEditor"
+        controls={["title", "my-block", "my-chip", "my-style"]}
+        customControls={[
+          {
+            name: "my-block",
+            icon: <TableChartIcon />,
+            type: "block",
+            blockWrapper: <MyBlock />
+          },
+          {
+            name: "my-chip",
+            icon: <TableViewIcon />,
+            type: "inline",
+            inlineStyle: {
+              borderWidth: "1px", 
+              borderColor: "primary",
+            }
+            // blockWrapper: <MyChip />
+          },
+          {
+            name: "my-style",
+            icon: <InvertColorsIcon />,
+            type: "inline",
+            inlineStyle: {
+                backgroundColor: "black",
+                color: "white"
+            }
+          }
+        ]}
+      />
+    </>
+  )
+}
+
+
+const MyBlock = (props: {children: ReactNode}) => {
+  return (
+      <div style={{
+          padding: 10,
+          backgroundColor: "#ebebeb"
+      }}>
+          My Block content is:
+          {props.children}
+      </div>
+  )
+}
+
