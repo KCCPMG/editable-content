94ff13bd75059dfa78a546b8669c1875db009ae6
Connor Wales <connorwales@gmail.com>
2024-09-05 23:58:37 -0700

    add ReactEditableDiv container rendering ReactParagraphs, bring onto app/page

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,23 @@ import ReactDynamicTextarea from "@/components/ReactDynamicTextarea";
 import MuiRte from "@/components/MuiRte";
 import ThemeProvider from "@mui/material/styles/ThemeProvider";
 import theme from "@/theme";
+import { ReactEditableDiv } from "@/components/ReactEditableDiv";
 
 export default function Home() {
   return (
-    <main>
+    <main
+      style={{
+        width: 500,
+        margin: "auto",
+        marginTop: 100
+      }}
+    >
       {/* <SlateInput /> */}
       {/* <SlateCustomElements /> */}
       {/* <SlateCustomFormatting /> */}
-      <EditableDiv />
+      {/* <EditableDiv /> */}
       {/* <ReactDynamicTextarea /> */}
+      <ReactEditableDiv initialContent="hello" />
       <ThemeProvider theme={theme}>
         {/* <MuiRte /> */}
       </ThemeProvider>
--- components/ReactEditableDiv.tsx
+++ components/ReactEditableDiv.tsx
@@ -3,23 +3,46 @@ import { useState, useRef, KeyboardEvent, FC, ReactElement } from 'react';
 
 
 type ReactEditableDivProps = {
-  initialContent: string | typeof ReactParagraph
+  initialContent: string | Array<ReactElement<typeof ReactParagraph>>
+}
+
+export function ReactEditableDiv({initialContent}: ReactEditableDivProps) {
+
+  const [content, setContent] = useState<Array<ReactElement<typeof ReactParagraph>>>(
+    typeof initialContent === "string" ? 
+    [<ReactParagraph initialContent={initialContent} />] :
+    initialContent
+
+  );
+
+  return (
+    <div 
+      style={{
+        width: "80%",
+        margin: "auto",
+        border: "2px solid black",
+      }}
+    >
+      {content}
+    </div>
+  )
+
 }
 
 
 
 type ReactParagraphProps = {
-  initialContent: string | ReactElement<typeof ReactSpan>
+  initialContent?: string | ReactElement<typeof ReactSpan>
 }
 
 export function ReactParagraph({initialContent} : ReactParagraphProps) {
   // stub
 
   const [contentType, setContentType] = useState<"string" | "ReactSpan">(
-    typeof initialContent === "string" ? "string" : "ReactSpan"
+    typeof initialContent === ("string" || undefined) ? "string" : "ReactSpan"
   )
 
-  const [content, setContent] = useState<string | ReactElement<typeof ReactSpan>>(initialContent);
+  const [content, setContent] = useState<string | ReactElement<typeof ReactSpan>>(initialContent || "");
 
 
   return (
