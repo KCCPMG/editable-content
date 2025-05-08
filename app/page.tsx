08be96f173f25ad39c9c7a9779fe9597a6e7de46
KCCPMG <39074889+KCCPMG@users.noreply.github.com>
2025-05-08 13:50:59 -0700

    clean up unused code in app/page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,4 @@
-import SlateInput from "@/components/SlateInput";
-import SlateCustomElements from "@/components/SlateCustomElements";
-import SlateCustomFormatting from "@/components/SlateCustomFormatting";
-import EditableDiv from "@/components/EditableDiv";
-import ReactDynamicTextarea from "@/components/ReactDynamicTextarea";
 import theme from "@/theme";
-import { ReactEditableDiv } from "@/components/ReactEditableDiv";
 import EditableContent from "@/components/EditableContent";
 import FormatBoldIcon from '@mui/icons-material/FormatBold';
 
@@ -17,15 +11,7 @@ export default function Home() {
         marginTop: 100
       }}
     >
-      {/* <SlateInput /> */}
-      {/* <SlateCustomElements /> */}
-      {/* <SlateCustomFormatting /> */}
-      {/* <EditableDiv /> */}
-      {/* <ReactDynamicTextarea /> */}
-      {/* <ReactEditableDiv initialContent="Hello" /> */}
-      <FormatBoldIcon/>
       <EditableContent 
-        // initialHTML="<strong>Test <i>Test</i> Test</strong>"
         initialHTML="Plain Text<strong>Strong Text</strong>"
         editTextButtons={[
           {
