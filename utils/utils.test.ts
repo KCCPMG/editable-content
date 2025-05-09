319858238697acbec31f5baa163512518baceb16
KCCPMG <39074889+KCCPMG@users.noreply.github.com>
2025-05-09 11:33:19 -0700

    install jest and related dependencies

---
+++ utils/utils.test.ts
@@ -0,0 +1,10 @@
+import {describe, expect, jest, test} from '@jest/globals';
+
+
+
+describe("basic test", () => {
+  test("basic test", () => {
+    console.log("ahhhhh");
+    expect(2).toBe(2);
+  })
+});
