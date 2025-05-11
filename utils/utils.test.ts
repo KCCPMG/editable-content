778678b104de3c8f49c768e7d649b81ccd7a6a58
KCCPMG <39074889+KCCPMG@users.noreply.github.com>
2025-05-10 23:19:01 -0700

    update test for setSelection, better format startingHTML to avoid empty text node creation

--- utils/utils.test.ts
+++ utils/utils.test.ts
@@ -7,20 +7,20 @@ import {describe, expect, jest, test, beforeAll} from '@jest/globals';
 import { setSelection } from './utils';
 
 
-const startingHTML = `
-<div>
+const startingHTML = 
+`<div>
   <strong id="strong-1">Strong Text</strong>
-  <i>Italics Test</i>
+  <i id="italics-1">Italics Test</i>
   Orphan Text
   <strong id="strong-2">
     Strong Text
-    <i>
+    <i id="italics-2">
       Strong and Italics Text
     </i>
     More Strong Text
   </strong>
 </div>
-`;
+`.replaceAll(/\n +/g, ''); // avoid empty text nodes from human-readable version above
 
 
 
@@ -50,12 +50,37 @@ describe("basic test", () => {
 
 describe("test setSelection", function() {
 
-  test("set Selection on text in node", function() {
+  test("set Selection on text inside italics node", function() {
     const italics = document.querySelector("i");
     expect(italics).not.toBeNull();
     const selection = setSelection(italics!, 0, italics!, italics!.childNodes.length);
     expect(selection?.toString()).toEqual(italics!.textContent);
+  })
 
+  test("set Selection on limited text inside italics node", function() {
+    const italics = document.querySelector("i#italics-2");
+    expect(italics).not.toBeNull();
+    const italicsText = italics!.childNodes[0];
+    console.log(italicsText.textContent);
+    console.log(italicsText.nodeName);
+    console.log(italicsText.nodeType);
+    const selection = setSelection(italicsText!, 7, italicsText!, 18);
+
+    const range = new Range();
+    range.setStart(italicsText, 7);
+    range.setEnd(italicsText, 18);
+    console.log(range.toString());
+    
+    range.setStart(italicsText, 0);
+    range.setEnd(italicsText, 7);
+    console.log(range.toString());
+    
+    console.log(italics?.childNodes.length);
+
+    expect(selection).not.toBeNull();
+    expect(selection!.anchorNode?.nodeName).toBe("#text");
+    expect(selection!.anchorNode).toBe(selection!.focusNode);
+    expect(selection!.toString()).toEqual("and Italics")
 
   })
 
