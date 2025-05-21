c599c0ada340e46d578ba4b80bb1bc54f84d11a0
KCCPMG <39074889+KCCPMG@users.noreply.github.com>
2025-05-21 10:52:15 -0700

    additional cleanup on EditTextButton

--- components/ContentEditableExperimentComponents/EditTextButton.tsx
+++ components/ContentEditableExperimentComponents/EditTextButton.tsx
@@ -1,8 +1,5 @@
-import React, { useState, useEffect } from "react";
 import { Button } from "@mui/material";
-import { ButtonOwnProps } from "@mui/material";
-import { EditTextButtonProps, WrapperArgs } from ".";
-
+import { EditTextButtonProps } from ".";
 
 
 
@@ -21,94 +18,10 @@ export default function EditTextButton({
   ...remainderProps}: EditTextButtonProps
 ) {
 
-  const [query, setQuery] = useState("");
-  const [currentColor, setCurrentColor] = useState<ButtonOwnProps['color']>(defaultColor || "primary");
-
-  
-
-  useEffect(() => {
-    
-    console.log("placeholder");
-    // const classList = wrapperClassList ? wrapperClassList.map(c => "."+c).join("") : "";
-    // const id = "#"+wrapperId;
-    // setQuery(wrapperElement + classList + id);
-
-  }, [element, classList, id])
-
-  // useEffect(() => {
-  //   console.log("selection listener useEffect");
-  //   getSelectionIsDescendentOf() ? setCurrentColor(selectedColor) : setCurrentColor(defaultColor)
-  // }, [selection?.anchorNode, selection?.focusNode, selection?.anchorOffset, selection?.focusOffset])
-
-  // console.log(selection?.toString());
-
-
-  // logic moved
-  // function createWrapper({wrapperElement, wrapperClassList, wrapperId}: WrapperArgs): HTMLElement {
-
-  //   const wrapper = document.createElement(wrapperElement);
-  //   if (wrapperClassList) {
-  //     wrapperClassList.forEach(wcl => {
-  //       wrapper.classList.add(wcl);
-  //     })
-  //   }
-  //   if (wrapperId) {
-  //     wrapper.setAttribute('id', wrapperId);
-  //   }
-
-  //   return wrapper;
-  // }
-  
-  
-  // function handleClick() {
-  //   // selection not descendent of wrapper, selection not covered
-  //   console.log(`click from EditTextButton ${dataKey}`)
-    
-  //   // TODO: purge any contents within of wrapper
-    
-    
-  //   if (!getSelectionIsDescendentOf() && !!selection) {
-      
-  //     const wrapper = createWrapper();
-  //     wrapInElement(selection, wrapper);
-
-  //     if (wrapper.childNodes.length === 0) {
-  //       const textNode = document.createTextNode("\u00A0");
-  //       wrapper.append(textNode);
-  //     }
-  //     selection.setBaseAndExtent(wrapper, 0, wrapper, wrapper.childNodes.length);
-
-  //   }
-
-    // TODO: selection not descendent of wrapper, selection covered
-
-
-    // TODO: selection descendent of wrapper
-
-
-    // TODO: Restore selection
-    // console.log(window.getSelection());
-    // !!selection && !!selection.anchorNode && !!selection.focusNode &&  selection.setBaseAndExtent(selection.anchorNode, selection.anchorOffset, selection.focusNode, selection.focusOffset);
-  // }
-
-
-  // function getSelectionIsDescendentOf(): boolean {
-  //   if (
-  //     !selection || 
-  //     !contentRef || 
-  //     !contentRef.current ||
-  //     query.trim() === ""
-  //   ) return false;
-  //   else return selectionIsDescendentOf(selection, query, contentRef.current);
-  // }
 
   return (
     isMUIButton ? 
       <Button 
-        // default, can be overidden
-        // variant="outlined"
-        // color={selected ? (selectedColor || "secondary") : (defaultColor || "primary") }
-        
         // regular button props
         {...remainderProps}
         
@@ -127,14 +40,6 @@ export default function EditTextButton({
       >
         {child}
       </button>
-    // <>
-    //   {/* <ListItemButton onClick={handleClick} selected>
-    //     {children}
-    //   </ListItemButton> */}
-    //   {/* <p>
-    //     Is Descendent Of: {String(getSelectionIsDescendentOf())}, Selection Covered By: {String(true)}
-    //   </p> */}
-    // </>
   )
 }
 
