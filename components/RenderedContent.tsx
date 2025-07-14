c6e0e6288dc93c3fe461ca943b937b43ab9484e7
Connor Wales <connorwales@gmail.com>
2025-07-14 14:27:56 -0700

    change portals render in RenderedContent to avoid unnecessary map call

--- components/RenderedContent.tsx
+++ components/RenderedContent.tsx
@@ -42,9 +42,7 @@ export default function RenderedContent({divStyle }: EditableContentProps) {
   useEffect(() => {
 
     setHasSelection(false);
-    // console.log("RenderedContent - useEffect on render")
-    // console.log(contentRefCurrentInnerHTML);
-    // console.log(contentRef.current);
+
     if (contentRef.current) {
       // if (initialHTML) {
       contentRef.current.innerHTML = dehydratedHTML;
@@ -54,40 +52,8 @@ export default function RenderedContent({divStyle }: EditableContentProps) {
       if (portals.length === 0) {
         reactContainerDivs.forEach(rcd => appendPortalToDiv(rcd as HTMLDivElement));
       } else resetPortalContainers();
-      // } else reHousePortals(reactContainerDivs);
-      
-        // } else {
-      //   contentRef.current.innerHTML = "";
-      // }   
-      // setContentRefCurrentInnerHTML(contentRef.current.innerHTML);
-    }
 
-    // populate div with html and update state
-    // if (contentRef.current) {
-    //   if (initialHTML) {
-    //     contentRef.current.innerHTML = initialHTML;
-        // // load react portals
-        // const reactContainerDivs = Array.from(contentRef.current.querySelectorAll("div [data-button-key]"));
-        // reactContainerDivs.forEach(rcd => appendPortalToDiv(rcd as HTMLDivElement));
-    //   } else {
-    //     contentRef.current.innerHTML = "";
-    //   }   
-    //   setContentRefCurrentInnerHTML(contentRef.current.innerHTML);
-    // }
-    
-    // assign event listeners
-    // document.addEventListener('selectionchange', (e) => {
-    //   const selection = window.getSelection();
-    //   if (!selection || 
-    //     !contentRef.current ||
-    //     !selectionIsDescendentOfNode(selection, contentRef.current)
-    //   ) {
-    //     updateSelection();
-    //   }
-    //   else {
-    //     handleSelectionChange();
-    //   } 
-    // })
+    }
 
     // teardown
     return () => {
@@ -99,127 +65,20 @@ export default function RenderedContent({divStyle }: EditableContentProps) {
 
   // on portal change
   useEffect(() => {
-    // updateContent();
     
     // clean up divs which no longer contain a portal
     if (!contentRef.current) return;
     const toDelete = Array.from(contentRef.current?.querySelectorAll("[data-mark-for-deletion]"));
 
     toDelete.forEach(td => promoteChildrenOfNode(td));
-    // resetSelectionToTextNodes();
 
   }, [portals])
 
-  // on divToSetSelectionTo change
-  // useEffect(() => {
-  //   // once react portal has rendered, set selection to text within, clear divToSetSelectionTo
-  //   if (divToSetSelectionTo) {
-  //     if (divToSetSelectionTo.childNodes.length > 0) {
-  //       window.getSelection()?.setBaseAndExtent(divToSetSelectionTo, 0, divToSetSelectionTo, divToSetSelectionTo.childNodes.length);
-  //       resetSelectionToTextNodes();
-  //       setDivToSetSelectionTo(null);
-  //     }
-  //   }
-  // }, [divToSetSelectionTo])
-
-
-  /**
-   * if changes need to be made to selection, make those changes, 
-   * otherwise update selection pieces of state
-   */
-  // function handleSelectionChange() {
-  //   const selection = window.getSelection();
-  //   if (selection && 
-  //     contentRef.current && 
-  //     selection?.anchorNode?.nodeType !== Node.TEXT_NODE &&
-  //     selection?.focusNode?.nodeType !== Node.TEXT_NODE
-  //   ) {
-  //     console.log("selectionHasTextNodes", selectionHasTextNodes(selection, contentRef.current));
-  //     if (selectionHasTextNodes(selection, contentRef.current)) {
-  //       resetSelectionToTextNodes();
-  //     } else {
-  //       const textNode = document.createTextNode('\u200B\u200B');
-  //       // contentRef.current.append(textNode);
-  //       const range = selection.getRangeAt(0);
-  //       range.insertNode(textNode)
-  //       range.setStart(textNode, 1);
-  //       range.setEnd(textNode, 1);
-  //       selection.removeAllRanges();
-  //       selection.addRange(range);
-  //     }
-  //   }
-  //   else {
-  //     updateSelection();
-  //   } 
-  // }
 
   return (
     <>
       <div
-        // contentEditable
         spellCheck={false}
-        // onInput={updateContent}
-        // onFocus={() => { setHasSelection(true) }}
-        // onBlur={() => { setHasSelection(false) }}
-        // onKeyDown={(e) => {
-        //   const selection = window.getSelection(); 
-        //   if (!selection || selection.rangeCount === 0 || !contentRef.current) return;
-        //   const range = selection.getRangeAt(0);
-
-        //   if (e.code === "Enter") {
-        //     e.preventDefault();
-        //     range.extractContents();
-            
-        //     const br = document.createElement("br");
-        //     const textNode = document.createTextNode('\u200B');
-        //     range.insertNode(textNode);
-        //     range.insertNode(br);
-            
-        //     range.setStart(textNode, 0);
-        //     range.setEnd(textNode, textNode.length);
-            
-        //     selection.removeAllRanges();
-        //     selection.addRange(range);  
-        //     updateContent(); 
-        //   }
-
-        //   if (e.code === "Space") { 
-        //     e.preventDefault();
-        //     const spaceNode = document.createTextNode("\u0020\u200B");
-        //     range.extractContents();
-        //     range.insertNode(spaceNode);
-        //     range.setStart(spaceNode, 2);
-        //     range.collapse();
-        //     selection.removeAllRanges();
-        //     selection.addRange(range);
-        //     updateContent();
-        //   }
-
-        //   if (e.code === "ArrowLeft") {
-        //     if (
-        //       !e.shiftKey &&
-        //       !e.altKey &&
-        //       !e.ctrlKey &&
-        //       !e.metaKey
-        //     ) {
-        //       e.preventDefault();
-        //       moveSelection(selection, contentRef.current, "left");
-        //     }
-        //   }
-
-        //   if (e.code === "ArrowRight") {
-        //     if (
-        //       !e.shiftKey &&
-        //       !e.altKey &&
-        //       !e.ctrlKey &&
-        //       !e.metaKey
-        //     ) {
-        //       e.preventDefault();
-        //       moveSelection(selection, contentRef.current, "right");
-        //     }
-        //   }    
-        // }}
-        // ref={contentRef}
         ref={assignContentRef}
         style={divStyle ? divStyle : {
           width: "100%",
@@ -230,7 +89,7 @@ export default function RenderedContent({divStyle }: EditableContentProps) {
         }}
       >
       </div>
-      {portals.map(portal => portal)}
+      {portals}
     </>
   )
 }
