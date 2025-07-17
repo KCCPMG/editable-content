2bcbb2260fe1455cca29696118eca900aceb95a3
Connor Wales <connorwales@gmail.com>
2025-07-16 19:21:52 -0700

    convert SelectionDisplay content to MUI Table

--- components/DisplayComponents/SelectionDisplay.tsx
+++ components/DisplayComponents/SelectionDisplay.tsx
@@ -1,5 +1,6 @@
 import { useEditableContentContext } from "@/context/EditableContentContext"
 import DisplayContainer from "./DisplayContainer";
+import { Table, TableRow, TableBody, TableCell} from "@mui/material";
 
 type SelectionToStringDisplayProps = {
   show?: boolean
@@ -17,15 +18,28 @@ export default function SelectionDisplay({show}: SelectionToStringDisplayProps) 
 
   return (
     <DisplayContainer title="Selection Text (selectionToString)" showInitial={!show}>
-      <strong>selectionToString:</strong>  { selectionToString }
-      <br/>
-      <strong>selectionAnchorNode:</strong> { stringRepresentationOfNode(selectionAnchorNode) }
-      <br/>
-      <strong>selectionAnchorOffset:</strong>  { selectionAnchorOffset }
-      <br/>
-      <strong>selectionFocusNode:</strong>  { stringRepresentationOfNode(selectionFocusNode) }
-      <br/>
-      <strong>selectionFocusOffset:</strong> { selectionFocusOffset }
+      <TableBody>
+        <TableRow>
+          <TableCell><strong>selectionToString:</strong></TableCell>
+          <TableCell>{ selectionToString }</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell><strong>selectionAnchorNode:</strong></TableCell>
+          <TableCell>{ stringRepresentationOfNode(selectionAnchorNode) }</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell><strong>selectionAnchorOffset:</strong></TableCell>
+          <TableCell>{ selectionAnchorOffset }</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell><strong>selectionFocusNode:</strong></TableCell>
+          <TableCell>{ stringRepresentationOfNode(selectionFocusNode) }</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell><strong>selectionFocusOffset:</strong></TableCell>
+          <TableCell>{ selectionFocusOffset }</TableCell>
+        </TableRow>
+      </TableBody> 
     </DisplayContainer>
   )
 
