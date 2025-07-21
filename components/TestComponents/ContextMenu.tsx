9dfd21c5d90dc23395b37c5bc329f3f2d28ff71c
Connor Wales <connorwales@gmail.com>
2025-07-21 10:58:56 -0700

    add uuid key to MenuItems rendered by Menu, build working after failure reported from Vercel

--- components/TestComponents/ContextMenu.tsx
+++ components/TestComponents/ContextMenu.tsx
@@ -1,5 +1,6 @@
 import { Box, Menu, MenuItem} from "@mui/material";
 import { useContextMenuContext } from "@/context/ContextMenuContext";
+import { v4 as uuidv4 } from 'uuid';
 
 
 type ContextMenuProps = {
@@ -22,7 +23,7 @@ export default function ContextMenu({
       <Menu open={showMenu}>
         {additionalMenuItemProps.map(item => {
           return (
-            <MenuItem {...item} />
+            <MenuItem key={uuidv4()} {...item} />
           )
         })}
       </Menu>
