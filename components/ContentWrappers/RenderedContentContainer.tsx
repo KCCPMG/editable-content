0c11a548e767594fa69c0711488dbaa4561da3db
KCCPMG <39074889+KCCPMG@users.noreply.github.com>
2025-10-09 14:02:33 -0700

    remove unnecessary and counterproductive useEffect, useLayoutEffect, import cleanup in RenderedContentContainer

--- components/ContentWrappers/RenderedContentContainer.tsx
+++ components/ContentWrappers/RenderedContentContainer.tsx
@@ -1,10 +1,8 @@
 "use client"
-import React, { useEffect, useLayoutEffect } from "react";
-import { RenderedContent, useEditableContentContext } from "editable-content";
+import React from "react";
+import { RenderedContent } from "editable-content";
 import { Paper, Typography, Button } from "@mui/material";
 import { Dispatch, SetStateAction } from "react";
-// import { promoteChildrenOfNode } from "editable-content/utils";
-
 
 
 type RenderedContentContainerProps = {
@@ -15,76 +13,6 @@ type RenderedContentContainerProps = {
 export default function RenderedContentContainer(
   {setEditMode}: RenderedContentContainerProps
 ) {
-
-  const {
-    contentRef, 
-    contentRefCurrentInnerHTML, 
-    setContentRefCurrentInnerHTML,
-    selectionToString, 
-    setSelectionToString,
-    selectionAnchorNode, 
-    setSelectionAnchorNode,
-    selectionAnchorOffset, 
-    setSelectionAnchorOffset,
-    selectionFocusNode, 
-    setSelectionFocusNode,
-    selectionFocusOffset, 
-    setSelectionFocusOffset,
-    hasSelection, 
-    setHasSelection,
-    portals, 
-    setPortals,
-    divToSetSelectionTo, 
-    setDivToSetSelectionTo,
-    appendPortalToDiv,
-    updateSelection,
-    updateContent,
-    dehydratedHTML,
-    resetPortalContainers,
-    assignContentRef
-  } = useEditableContentContext();
-
-
-
-  // on initial render
-  useLayoutEffect(() => {
-
-    setHasSelection(false);
-
-    if (contentRef.current) {
-      // if (initialHTML) {
-      contentRef.current.innerHTML = dehydratedHTML;
-      
-      console.log("portals.length:", portals.length);
-
-      // load react portals
-      const reactContainerDivs = Array.from(contentRef.current.querySelectorAll("div [data-button-key]")) as Array<HTMLDivElement>;
-      if (portals.length === 0) {
-        reactContainerDivs.forEach(rcd => appendPortalToDiv(rcd as HTMLDivElement));
-      } else resetPortalContainers();
-
-    }
-
-    // teardown
-    return () => {
-      contentRef.current = null;
-      // document.removeEventListener('selectionchange', handleSelectionChange);
-    }
-
-  }, [contentRef])
-
-
-  // on portal change
-  // useEffect(() => {
-    
-  //   // clean up divs which no longer contain a portal
-  //   if (!contentRef.current) return;
-  //   const toDelete = Array.from(contentRef.current?.querySelectorAll("[data-mark-for-deletion]"));
-
-  //   toDelete.forEach(td => promoteChildrenOfNode(td));
-
-  // }, [portals])
-
 
   return (
     <Paper 
@@ -99,23 +27,6 @@ export default function RenderedContentContainer(
     >
       <Typography variant="h5">Rendered Content</Typography>
       <hr/>
-      {/* <div
-        spellCheck={false}
-        ref={assignContentRef}
-        style={{
-          height: "250px",
-          overflowY: "scroll"
-        }}
-        // style={divStyle ? divStyle : {
-        //   width: "100%",
-        //   height: "150px",
-        //   margin: "auto",
-        //   // border: "2px solid black",
-        //   overflowY: "scroll"
-        // }}
-      >
-      </div>
-      {portals} */}
       <RenderedContent />
       <hr/>
       <Button onClick={() => setEditMode(true)}>
