eed69f31108933d9f77f2f7c388f6c83c0baf0ed
KCCPMG <39074889+KCCPMG@users.noreply.github.com>
2025-10-18 22:56:32 -0700

    create readme page, incl table of contents and content

---
+++ app/readme/page.tsx
@@ -0,0 +1,14 @@
+import Contents from "./Contents";
+import ReadmeContent from "./ReadmeContent";
+
+
+export default function Page() {
+  return (
+    <>
+      <h1>README</h1>
+      <h3>Contents</h3>
+      <Contents />
+      <ReadmeContent />
+    </>
+  )
+}
