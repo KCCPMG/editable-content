cd28652169ceb61f7861a35d0dd99d9dfe3f6b86
KCCPMG <39074889+KCCPMG@users.noreply.github.com>
2025-10-25 13:06:00 -0700

    dynamically assign slugs to headings in Content

--- app/documentation/Content.tsx
+++ app/documentation/Content.tsx
@@ -1,8 +1,10 @@
 "use client";
 import ReactMarkdown from "react-markdown";
-
+import GithubSlugger from 'github-slugger';
 
 export default function Content({stringified}: {stringified: string}) {
+
+  const slugger = new GithubSlugger(); 
 
   return (
     <ReactMarkdown
@@ -52,6 +54,70 @@ export default function Content({stringified}: {stringified: string}) {
               )
             } else return (
               <pre {...rest} children={children} />
+            )
+          }
+        },
+        h1(props) {
+          const {children, node, ...rest} = props;
+          if (children && typeof children === "string") {
+            return (
+              <h1 id={slugger.slug(children)} {...rest}>
+                {children}
+              </h1>
+            )
+          } else {
+            return (
+              <h1 {...rest}>
+                {children}
+              </h1>
+            )
+          }
+        },
+        h2(props) {
+          const {children, node, ...rest} = props;
+          if (children && typeof children === "string") {
+            return (
+              <h2 id={slugger.slug(children)} {...rest}>
+                {children}
+              </h2>
+            )
+          } else {
+            return (
+              <h2 {...rest}>
+                {children}
+              </h2>
+            )
+          }
+        },
+        h3(props) {
+          const {children, node, ...rest} = props;
+          if (children && typeof children === "string") {
+            return (
+              <h3 id={slugger.slug(children)} {...rest}>
+                {children}
+              </h3>
+            )
+          } else {
+            return (
+              <h3 {...rest}>
+                {children}
+              </h3>
+            )
+          }
+        },
+        h4(props) {
+          const {children, node, ...rest} = props;
+          if (children && typeof children === "string") {
+            return (
+              <h4 id={slugger.slug(children)} {...rest}>
+                {children}
+              </h4>
+            )
+          } else {
+            return (
+              <h4 {...rest}>
+                {children}
+              </h4>
             )
           }
         }
