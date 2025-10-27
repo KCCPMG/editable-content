58b0bf57b89522ec0f9cf44f8a0e8f77d929f4b2
KCCPMG <39074889+KCCPMG@users.noreply.github.com>
2025-10-27 16:15:38 -0700

    clean up description text on stateful-and-propful page

--- app/stateful-and-propful/page.tsx
+++ app/stateful-and-propful/page.tsx
@@ -18,19 +18,34 @@ export default function Page() {
 
   const [initialClicks, setInitialClicks] = useState(0);
   const [componentBorderColor, setComponentBorderColor] = useState("Red");
-  const [editMode, setEditMode] = useState<boolean>(false);
+  const [editMode, setEditMode] = useState<boolean>(true);
 
   return (
     <>
       <Typography variant="h2">Stateful and Propful Components</Typography>
       <p>
-        Since we are using React, we presumably want our components to have the ability to change. Generally speaking, the two ways that that is done are through changes to a component&apos;s props or its state. Here we have two different components which handle those changes differently.
+        Since we are using React, we presumably want our components to have the ability to change. Generally speaking, the two most common ways that that is done are through changes to a component&apos;s props or its state. Here we have two different components which handle those changes differently.
       </p>
       <p>
-        There are two components here: <strong>Stateful and Propful</strong> and <strong>Stateful Only</strong>. Both of these manage as state the number of times that they have been clicked, but <strong>Stateful and Propful</strong> will also take a border color as a prop, and the border color can be changed for all instances of <strong>Stateful and Propful</strong> by clicking the <strong>Rotate Color From...</strong> Button.
+        There are two components here: <strong>Stateful and Propful</strong> and <strong>Stateful Only</strong>. Both of these component types will keep track of the number of times that they have been clicked in their own state. However, <strong>Stateful and Propful</strong> 
+        also has its border color set by a prop. Try clicking on the <strong>Rotate Color From...</strong> Button to see all instances of the <strong>Stateful and Propful</strong> change their border color. Note that the change border color will persist when clicking the <strong>Render Text</strong> button and then the <strong>Edit Text</strong> button again. This means that the border color prop is stable across re-renders.
       </p>
       <p>
         What you should observe is that clicking the <strong>Rotate Color From...</strong> Button changes the border color of every instance of <strong>Stateful and Propful</strong>. These changes will not only persist when switching back and forth between <strong>Edit Text</strong> and <strong>Render Text</strong>, all new <strong>Stateful and Propful</strong> components will have the same border color when they are created.
+      </p>
+      <p>
+        However, as you go back and forth between the text being rendered and the text being edited, you will see that the click count is not being preserved in the same way. The prior click count will remain, but a new click count will also appear as 0. For example, if you click the Stateful Only component 7 times, the component will read as &ldquo;7 Stateful Only Component&rdquo;. Clicking the &ldquo;Render Text Button&rdquo; will then change this to &ldquo;0 7 Stateful Only Component&rdquo;. There are two separate things happening here:
+        <ul>
+          <li>
+            The re-rendering of the component will cause it to reinitialize its click count state as 0, which it will then display at the beginning of the component.
+          </li>
+          <li>
+            The previous click count of &ldquo;7&rdquo; is absorbed into the text that is being rendered.
+          </li>
+        </ul>
+      </p>
+      <p>
+        What this illustrates is that components are re-rendered by switching. This destroys the state, but props are preserved at a higher level. The next example will show a better way of managing this situation with a stable click count.
       </p>
       <p>
         Additionally, clicking any of the rendered components should increase the click count that is displayed in that component, BUT, you will note that that number is reset every time when switching back and forth between <strong>Edit Text</strong> and <strong>Render Text</strong>. 
@@ -38,17 +53,6 @@ export default function Page() {
       <p>
         <strong>Note:</strong> When switching back and forth, the previous count will still show, meaning more and more numbers will appear. This gives you a record of what the previous state was, but also demonstrates why it is not a good idea to render additional text in your wrappers.
       </p>
-      {/* <p>
-        There are two values here which are passed as props to every StatefulAndPropfulBox that is rendered here. What I expect to happen is that changing these props at this level (which can be done with the buttons) will have two distinct outcomes: 
-      </p>
-      <ul>
-        <li>
-          Clicking the &quot;Rotate Color From...&quot; button should change the props of all &quot;Stateful and Propful&quot; components, as these change a state value upstream which then updates the props of all found portal instances (see the logic in the useEffect hook). Each component&apos;s border color should immediately change as the component&apos;s background color is the prop passed to it.
-        </li>
-        <li>
-          Clicking the &quot;Increase Clicks From...&quot; button will also change an upstream state value and this should also change the props, of each &quot;Stateful and Propful&quot; component, but the value displayed for the clicks of that component should not change, as the component&apos;s click count is determined by its internal state. However, future &quot;Stateful and Propful&quot; components that are created should all start from whatever value is held at the time from &quot;Increase Clicks From...&quot;
-        </li>
-      </ul> */}
       <EditableContentContextProvider
         initialHTML={initialHTML}
         keyAndWrapperObjs={[
