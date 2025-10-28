d77e0cdff7e95bca4638fefda14efbd68334ff55
KCCPMG <39074889+KCCPMG@users.noreply.github.com>
2025-10-27 19:03:17 -0700

    redo propful-only/PropfulOnlyContent to match style of ButtonAndContentContainer

--- app/propful-only/PropfulOnlyContent.tsx
+++ app/propful-only/PropfulOnlyContent.tsx
@@ -1,10 +1,13 @@
 import AllPropsDisplay from "@/components/DisplayComponents/AllPropsDisplay";
-import { EditableContentContext, EditableContent, EditTextButton, RenderedContent } from "@kccpmg/editable-content"; 
+import { EditableContentContext, EditableContent, EditTextButton, RenderedContent } from "@kccpmg/editable-content";
 import { FormatBold, FormatItalic } from "@mui/icons-material";
-import { Box, Button, Container } from "@mui/material";
+import { Box, Button, Container, Divider, Paper, Typography } from "@mui/material";
 import IncreaseColorButton from "./IncreaseColorButton";
 import { useContext, useState } from "react";
 import { EditModeContext } from "@/context/EditModeContext";
+import ClearButton from "@/components/ButtonsAndUI/ClearButton";
+import styles from "@/components/ContentWrappers/ButtonAndContentContainer.module.css";
+import RenderedContentContainer from "@/components/ContentWrappers/RenderedContentContainer";
 
 
 
@@ -22,9 +25,13 @@ export default function PropfulOnlyContent() {
 
   return (
     <>
-      {editMode &&
+      {editMode ?
         <Box>
-          <Box>
+          <Box 
+            sx={{
+              paddingBottom: "16px"
+            }}
+          >
             <h4>
               Change Props For All Components
             </h4>
@@ -35,68 +42,113 @@ export default function PropfulOnlyContent() {
           </Box>
           <Box
             sx={{
-              display: "flex",
-              flexDirection: "row",
-              gap: "4",
-              alignItems: "flex-start",
-              flexWrap: "wrap",
-              justifyContent: "left"
+              border: "2px solid black",
+              borderRadius: "8px",
+              overflow: 'hidden',
+              // padding: '2px',
+              paddingRight: '10px'
             }}
           >
             <Box
+              className="all-buttons-box"
               sx={{
-                width: 'fit-content',
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'flex-start',
-                gap: 1
+                display: "flex",
+                flexDirection: "row",
+                gap: "4",
+                alignItems: "flex-start",
+                flexWrap: "wrap",
+                justifyContent: "left",
+                backgroundColor: "#fafaf1",
+                paddingTop: '2px',
+                marginRight: '-8px'
+                // internalPadding: "4px"
               }}
             >
-              <h4>React Buttons</h4>
-              <Box>
-                <EditTextButton
-                  isMUIButton={true}
-                  dataKey="propful-only"
-                >
-                  Propful Box
-                </EditTextButton>
-                <EditTextButton
-                  isMUIButton={true}
-                  dataKey="stateful-component"
-                >
-                  Stateful Box
-                </EditTextButton>
+              <Box
+                sx={{
+                  width: 'fit-content',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'flex-start',
+                  gap: 1
+                }}
+              >
+                <h4 style={{ margin: 0, marginTop: 4, marginLeft: 4 }}>React Buttons</h4>
+                <Box display="flex">
+                  <EditTextButton
+                    isMUIButton={true}
+                    dataKey="propful-only"
+                    deselectedVariant="text"
+                  >
+                    Propful Box
+                  </EditTextButton>
+                  <Divider orientation="vertical" flexItem />
+                  <EditTextButton
+                    isMUIButton={true}
+                    dataKey="stateful-component"
+                    deselectedVariant="text"
+                  >
+                    Stateful Box
+                  </EditTextButton>
+                </Box>
               </Box>
             </Box>
+            <EditableContent
+              className={styles.ButtonAndContentContainerEditableContent}
+            />
             <Box
+              className="all-buttons-box"
               sx={{
-                width: 'fit-content',
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'flex-start',
-                gap: 1
+                display: "flex",
+                flexDirection: "row",
+                gap: "4",
+                alignItems: "flex-start",
+                flexWrap: "wrap",
+                justifyContent: "right",
+                backgroundColor: "#fafaf1",
+                paddingTop: '2px',
+                marginRight: '-8px'
+                // internalPadding: "4px"
               }}
             >
-              <h4>Non-React Buttons</h4>
-              <Box>
-                <EditTextButton
-                  isMUIButton={true}
-                  dataKey="strong"
-                >
-                  <FormatBold />
-                </EditTextButton>
-                <EditTextButton
-                  isMUIButton={true}
-                  dataKey="standard-italics"
-                >
-                  <FormatItalic />
-                </EditTextButton>
-              </Box>
+              <ClearButton />
+              <Divider
+                orientation="vertical"
+                flexItem
+                sx={{
+                  margin: '4px'
+                }}
+              />
+              <Button variant="text" onClick={
+                () => setEditMode(false)
+              }>
+                Render Text
+              </Button>
             </Box>
           </Box>
         </Box>
+
+        :
+        <Paper
+          elevation={16}
+          sx={{
+            border: "1px solid black",
+            borderRadius: "8px",
+            overflow: 'hidden',
+            padding: '4px',
+            paddingRight: '10px'
+          }}
+        >
+          <Typography variant="h5">Rendered Content</Typography>
+          <hr />
+          <RenderedContent />
+          <hr />
+          <Button onClick={() => setEditMode(true)}>
+            Edit Text
+          </Button>
+        </Paper>
       }
-      {editMode ?
+      {/* {editMode ?
         <EditableContent
           className="default-editable-content"
         /> :
@@ -106,7 +158,7 @@ export default function PropfulOnlyContent() {
       }
       <Button variant="outlined" onClick={() => setEditMode(!editMode)}>
         {editMode ? "Render Text" : "Edit Text"}
-      </Button>
+      </Button> */}
       <Container>
         <AllPropsDisplay show={true} />
       </Container>
