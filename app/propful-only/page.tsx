929806ed4533a199ad5735d43ecc23594e824149
KCCPMG <39074889+KCCPMG@users.noreply.github.com>
2025-10-27 19:16:08 -0700

    add note to propful-only/page explaining exclude-from-dehydrated

--- app/propful-only/page.tsx
+++ app/propful-only/page.tsx
@@ -33,6 +33,9 @@ export default function Page() {
       <p>
         The text that is already wrapped here is in a <strong>Propful Box</strong>. You can create more of these or create a <strong>Stateful Box</strong> for comparison. What you will see with the <strong>Propful Box</strong> is that in addition to the color persisting as it did in the previous example, the number of clicks will also persist when going back and forth between <strong>Edit Text</strong> and <strong>Render Text</strong>. You can also see the props that are passed to all elements by expanding the <strong>All Props</strong> display container at the bottom of the page. This display uses the <code>getAllPortalProps</code> function which is passed through the <code>EditableContentContext</code> and allows developers to extract props to save for future hydration.
       </p>
+      <p>
+        <strong>Note:</strong> this example is also not affected by the insertion of new numbers as the user switches back and from from editable and rendered text (as was seen in the prior example). This is not due to the switch to a Propful Only component, but is due to another fix related to the <code>EXCLUDE_FROM_DEHYDRATED</code> value. For more information on this, see the documentation.
+      </p>
       <EditModeContextProvider initialEditMode={true}>
         <EditableContentContextProvider
           keyAndWrapperObjs={[
