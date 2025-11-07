30c8b666cd104af0492d1790bf02a674667d76ba
KCCPMG <39074889+KCCPMG@users.noreply.github.com>
2025-11-07 15:08:17 -0800

    create logic in layout.tsx to check user-agent for os

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,41 @@ export const metadata: Metadata = {
 };
 
 
+const mobileOSs = [
+  "android",
+  "ios"
+]
+
+function getSystemInformation(userAgentStr: string) {
+  const parentheticalOpening = userAgentStr.indexOf("(");
+  if (parentheticalOpening === -1) return "";
+  const parentheticalClosing = userAgentStr.indexOf(")", parentheticalOpening);
+  if (parentheticalClosing === -1) return "";
+  return userAgentStr.slice(parentheticalOpening, parentheticalClosing+1);
+}
+
+function timeFunctions(testString: string) {
+  const getSystemInformationStartTime = performance.now();
+  const getSIString = getSystemInformation(testString);
+  const getSystemInformationEndTime = performance.now();
+  
+  const regExStartTime = performance.now();
+  const reMatch = testString.match(/\(.*?\)/)
+  const matchedPattern = reMatch ? reMatch[0] : "";
+  const regExEndTime = performance.now();
+
+  console.log(
+    "getSystemInformation:", 
+    getSIString,
+    getSystemInformationEndTime-getSystemInformationStartTime,
+    "\nregEx:", 
+    matchedPattern,
+    regExEndTime-regExStartTime
+  );
+
+}
+
+
 
 export default async function RootLayout({
   children,
@@ -36,17 +71,34 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   
-  const reqHeaders = await headers();
+  const reqHeaders = headers();
 
-  for (let pair of reqHeaders.entries()) {
-    console.log(pair);
+  const userAgent = reqHeaders.get('user-agent');
+  const systemInformation = userAgent ? getSystemInformation(userAgent).toLowerCase() : "";
+
+
+  console.log(systemInformation);
+
+  let assumeIsMobile = false;
+
+  for (let mobileOS of mobileOSs) {
+    if (systemInformation.indexOf(mobileOS) >= 0) {
+      assumeIsMobile = true;
+      break;
+    }
   }
+
+  // for (let pair of reqHeaders.entries()) {
+  //   console.log(pair);
+  // }
 
   return (
     <html lang="en" className={`${montserrat.variable} ${roboto.variable}`}>
       <body>
         <ThemeProvider theme={theme}>
-          <ClientLayout>
+          <ClientLayout 
+            // assumeIsMobile={assumeIsMobile}
+          >
             {children}
           </ClientLayout>
         </ThemeProvider>
